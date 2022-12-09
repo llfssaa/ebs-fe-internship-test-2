3230120074106a966719118a935cc8099bad4ffc
llfssaa <liubovitopolcean@gmail.com>
2022-12-09 13:57:00 +0200

    add calculate logic

--- src/ui/molecules/Calculator/Calculator.tsx
+++ src/ui/molecules/Calculator/Calculator.tsx
@@ -5,47 +5,72 @@ import './calculator.scss';
 
 function Calculator() {
   const [screenValue, setScreenValue] = React.useState('0');
-  const [sign, setSign] = React.useState('');
+  const [sign, setSign] = React.useState<string>('');
 
   const screenValuePrev = React.useRef(screenValue);
+  const mainLogic = (value: string, prevVal: string, signBtn: string): void => {
+    switch (signBtn) {
+      case '+':
+        setScreenValue((Number(prevVal) + Number(value)).toString());
+        break;
+      case '-':
+        setScreenValue((Number(prevVal) - Number(value)).toString());
+        break;
+      case '*':
+        setScreenValue((Number(prevVal) * Number(value)).toString());
+        break;
+      case '/':
+        setScreenValue((Number(prevVal) / Number(value)).toString());
+        break;
+      case '%':
+        setScreenValue((Number(prevVal) % Number(value)).toString());
+        break;
+      case 'AC':
+        setScreenValue('0');
+        break;
+      default:
+        setScreenValue(value);
+    }
+  };
   const keyboardHandler = (value: string): void => {
     console.log(value);
     const btnNum = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     if (btnNum.includes(value)) {
-      if (screenValue === '0') {
+      if (
+        screenValue === '0' ||
+        screenValue.includes('+') ||
+        screenValue.includes('-') ||
+        screenValue.includes('*') ||
+        screenValue.includes('/') ||
+        screenValue.includes('%')
+      ) {
         setScreenValue(value);
       } else {
         setScreenValue(screenValue + value);
       }
     }
-    if (!btnNum.includes(value) && value !== '=') {
+    if (value === '.') {
+      if (screenValue === '0') {
+        setScreenValue('0.');
+      } else {
+        setScreenValue(screenValue + value);
+      }
+    }
+    if (value === '+/-' && screenValue !== '0') {
+      setScreenValue((Number(screenValue) * -1).toString());
+    }
+    if (!btnNum.includes(value) && value !== '=' && value !== '.' && value !== '+/-' && value !== 'AC') {
       screenValuePrev.current = screenValue;
       setSign(value);
-      setScreenValue('0');
+      setScreenValue(value);
     }
-    if (value === '=') {
-      if (sign === '+') {
-        setScreenValue((Number(screenValuePrev.current) + Number(screenValue)).toString());
-      }
-      if (sign === '-') {
-        setScreenValue((Number(screenValuePrev.current) - Number(screenValue)).toString());
-      }
-      if (sign === '*') {
-        setScreenValue((Number(screenValuePrev.current) * Number(screenValue)).toString());
-      }
-      if (sign === '/') {
-        setScreenValue((Number(screenValuePrev.current) / Number(screenValue)).toString());
-      }
-      if (sign === '%') {
-        setScreenValue((Number(screenValuePrev.current) % Number(screenValue)).toString());
-      }
-      if (sign === '+/-') {
-        setScreenValue((Number(screenValuePrev.current) * -1).toString());
-      }
-      if (sign === 'AC') {
-        setScreenValue('0');
-      }
+    if (value === 'AC') {
+      setScreenValue('0');
+      setSign('');
+    }
 
+    if (value === '=') {
+      mainLogic(screenValue, screenValuePrev.current, sign);
       setSign('');
     }
     console.log(`sign= ${sign}`);
