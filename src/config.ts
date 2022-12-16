c8a4dbc3f1698da3931a6074da46a9800544e45f
llfssaa <liubovitopolcean@gmail.com>
2022-12-16 14:53:54 +0200

    add calc logic

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,16 @@
 // eslint-disable-next-line import/prefer-default-export
 export const btnNum = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+export enum SpecialSign {
+  Dot = '.',
+  PlusMinus = '+/-',
+  AllClear = 'AC',
+  Equal = '=',
+}
+export enum Sign {
+  Plus = '+',
+  Minus = '-',
+  Multiply = '*',
+  Divide = '/',
+  Percent = '%',
+}
--- src/ui/molecules/Calculator/Calculator.tsx
+++ src/ui/molecules/Calculator/Calculator.tsx
@@ -2,72 +2,91 @@ import React from 'react';
 import Screen from '../../atoms/Screen/Screen';
 import ButtonBox from '../ButtonBox/ButtonBox';
 import './calculator.scss';
-import { btnNum } from '../../../config';
+import { btnNum, Sign, SpecialSign } from '../../../config';
 import { isSign } from '../../../helpers';
 
 function Calculator() {
   const [screenValue, setScreenValue] = React.useState('0');
   const [sign, setSign] = React.useState<string>('');
 
+  console.log('screenValue ', screenValue);
+
   const screenValuePrev = React.useRef(screenValue);
-  const mainLogic = (value: string, prevVal: string, signBtn: string): void => {
+  const calcValue = React.useRef({ first: 0, second: 0 });
+  const isSetSign = () => {
+    return sign ? 'first' : 'second';
+  };
+  const mainLogic = (value: number, prevVal: number, signBtn: string): string => {
     switch (signBtn) {
-      case '+':
-        setScreenValue((Number(prevVal) + Number(value)).toString());
-        break;
-      case '-':
-        setScreenValue((Number(prevVal) - Number(value)).toString());
-        break;
-      case '*':
-        setScreenValue((Number(prevVal) * Number(value)).toString());
-        break;
-      case '/':
-        setScreenValue((Number(prevVal) / Number(value)).toString());
-        break;
-      case '%':
-        setScreenValue((Number(prevVal) % Number(value)).toString());
-        break;
-      case 'AC':
-        setScreenValue('0');
-        break;
+      case Sign.Plus:
+        return (prevVal + value).toString().length > 9
+          ? (prevVal + value).toPrecision(5)
+          : (prevVal + value).toString();
+      case Sign.Minus:
+        return (prevVal - value).toString().length > 9
+          ? (prevVal - value).toPrecision(5)
+          : (prevVal - value).toString();
+      case Sign.Multiply:
+        return (prevVal * value).toString().length > 9
+          ? (prevVal * value).toPrecision(5)
+          : (prevVal * value).toString();
+      case Sign.Divide:
+        return (prevVal / value).toString().length > 9
+          ? (prevVal / value).toPrecision(5)
+          : (prevVal / value).toString();
+      case Sign.Percent:
+        return (prevVal % value).toString().length > 9
+          ? (prevVal % value).toPrecision(5)
+          : (prevVal % value).toString();
       default:
-        setScreenValue(value);
+        return value.toString();
     }
   };
+  /* const calculatorLogic = (value: string): void => {}; */
   const screenDriver = (value: string) => {
+    if (screenValue.length > 9 && btnNum.includes(value)) return;
     if (btnNum.includes(value)) {
       if (screenValue === '0' || isSign(screenValue)) {
         setScreenValue(value);
+        calcValue.current[isSetSign()] = Number(value);
       } else {
         setScreenValue((prev) => prev + value);
+        calcValue.current[isSetSign()] = Number(screenValue + value);
       }
     }
-    if (value === '.') {
+    if (value === SpecialSign.Dot) {
       if (screenValue === '0') {
         setScreenValue('0.');
+        calcValue.current[isSetSign()] = Number(screenValue);
       } else {
         setScreenValue((prev) => prev + value);
+        calcValue.current[isSetSign()] = Number(screenValue + value);
       }
     }
-    if (value === '+/-' && screenValue !== '0') {
+    if (value === SpecialSign.PlusMinus && screenValue !== '0') {
       setScreenValue((Number(screenValue) * -1).toString());
+      calcValue.current[isSetSign()] = calcValue.current[isSetSign()] * -1;
     }
-    if (!btnNum.includes(value) && value !== '=' && value !== '.' && value !== '+/-' && value !== 'AC') {
+    if (!btnNum.includes(value) && isSign(value)) {
       screenValuePrev.current = screenValue;
       setSign(value);
       setScreenValue(value);
     }
-    if (value === 'AC') {
+    if (value === SpecialSign.AllClear) {
       setScreenValue('0');
       setSign('');
+      calcValue.current = { first: 0, second: 0 };
     }
   };
   const keyboardHandler = (value: string): void => {
-    screenDriver(value);
-    if (value === '=') {
-      mainLogic(screenValue, screenValuePrev.current, sign);
+    if (value === SpecialSign.Equal) {
       setSign('');
+      setScreenValue(mainLogic(calcValue.current.first, calcValue.current.second, sign));
+      if (sign === '') {
+        setScreenValue('0');
+      }
     }
+    screenDriver(value);
   };
   return (
     <div className="wrapper">
